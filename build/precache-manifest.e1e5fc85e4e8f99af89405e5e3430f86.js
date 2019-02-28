eb0a01bbd9471e031d7d5d062e6a86397cd96189
Isaac Oh <isaacohishere@gmail.com>
2019-02-28 02:20:18 -0800

    Build to master

---
+++ build/precache-manifest.e1e5fc85e4e8f99af89405e5e3430f86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d34227a58fbe6117cb63",
+    "url": "/kokio-chicken/static/js/runtime~main.d34227a5.js"
+  },
+  {
+    "revision": "7e2fc20854d70afd0f40",
+    "url": "/kokio-chicken/static/js/main.7e2fc208.chunk.js"
+  },
+  {
+    "revision": "fbb47d24e31039aef4a9",
+    "url": "/kokio-chicken/static/js/2.fbb47d24.chunk.js"
+  },
+  {
+    "revision": "7e2fc20854d70afd0f40",
+    "url": "/kokio-chicken/static/css/main.9d68521e.chunk.css"
+  },
+  {
+    "revision": "a060af706be9be052abeb500c0d13279",
+    "url": "/kokio-chicken/index.html"
+  }
+];
